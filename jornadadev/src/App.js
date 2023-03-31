3d30e49cbdec730b4dcc20331a08f338f9022189
André_Dev <84449123+andre-dev-2021@users.noreply.github.com>
2023-03-31 23:42:00 +0000

    Dia 04 - Sidebar e preparação para DB

--- jornadadev/src/App.js
+++ jornadadev/src/App.js
@@ -5,10 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video 
+          likes={111}
+          messages={222}
+          shares={333}
+          name="Andre"
+          description="Miaaaauuuuu!"
+          music="ihhhhhhhhhhhhh"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video 
+          likes={444}
+          messages={555}
+          shares={666}
+          name="John Doe"
+          description="Bing!"
+          music="clap clap clap clap clap"
+          url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/bird.mp4?alt=media&token=52abbeec-ff95-4acb-808e-5a4b4977d1da"
+        />
       </div>
     </div>
   );
